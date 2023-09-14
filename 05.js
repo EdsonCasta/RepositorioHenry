bfabcaa4bfb77a1f0a21aa02d50d3b8658c84d0d
EdsonCasta <dedsoncasta07@gmail.com>
2023-09-13 22:16:35 -0500

    simulacion henry challenge

---
+++ 05.js
@@ -0,0 +1,34 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function pluck(array, propiedad) {
+  // La función llamada 'pluck' recibe como argumento un array de objetos llamado 'array' y el nombre de una
+  // propiedad.
+  // La función debe devolver un nuevo arreglo con solo los valores dentro de la propiedad recibida
+  // Ej:
+  // var productos = [{ name: 'TV LCD', price: 100}, { name: 'Computadora', price: 500 }]
+  // productos.pluck(productos, 'name') debería devolver ['TV LCD', 'Computadora']
+  // Pista: es una buena oportunidad para usar map.
+
+  // Tu código acá:
+  // var nuevoArray = array.map(function(elemento){
+  //   return elemento[propiedad];
+  // });
+  // return nuevoArray;
+  var nuevoArray = [];
+
+  for(var i = 0; i < array.length; i++){
+    var productos = array[i][propiedad];
+    nuevoArray.push(productos);
+  }
+  return nuevoArray;
+}
+
+console.log(pluck([{ name: 'TV LCD', price: 100}, { name: 'Computadora', price: 500 }], 'name'));
+
+// No modifiques nada debajo de esta linea //
+
+module.exports = pluck
